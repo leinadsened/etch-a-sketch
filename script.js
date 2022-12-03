26461a68402313a22d2effcf3ec745fbce8fb35d
leinadsened <denes.daniel93@gmail.com>
2022-12-03 13:25:21 +0100

    add adjustable grid

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let container = document.getElementById("container");
-let gridSize = 16;
+let gridSize = prompt("How big of a grid would you like good sire?", Number);
 
 for (i=1; i <= gridSize; i++){
     let gridRow = document.createElement("div");
+    let gridHeight = 100/gridSize + "%";
+    gridRow.style.height = gridHeight;
     for (j=1; j <= gridSize; j++){
         let gridCell = document.createElement("div");
+        let gridWidth = 100/gridSize + "%";
         gridCell.classList.add("grid-cell");
+        gridCell.style.width = gridWidth;
         gridCell.addEventListener("mouseover", () => gridCell.classList.add("grid-cell-hovered"));
         gridRow.appendChild(gridCell);
     }
