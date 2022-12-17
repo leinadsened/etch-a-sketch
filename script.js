115a135384f70b470d76766577cb21b32047c7b5
leinadsened <denes.daniel93@gmail.com>
2022-12-17 13:13:05 +0100

    switch to hsl, remove adjustable grid

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 let container = document.getElementById("container");
-let gridSize = 0;
-let check = checkNumber();
-while(check){
-    gridSize = prompt("How big of a grid would you like good sire?", "16");
-    check = checkNumber();
-}
-function checkNumber(){
-    if (parseInt(gridSize) > 1 && parseInt(gridSize) < 100){
-        return false;
-    } else {
-        return true;
-    }
-}
+let gridSize = 16;
+
 for (i=1; i <= gridSize; i++){
     let gridRow = document.createElement("div");
     let gridHeight = 100/gridSize + "%";
@@ -21,8 +10,8 @@ for (i=1; i <= gridSize; i++){
         let gridWidth = 100/gridSize + "%";
         gridCell.classList.add("grid-cell");
         gridCell.style.width = gridWidth;
-        gridCell.addEventListener("mouseover", () => {let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        gridCell.style.background = "#" + randomColor;});
+        gridCell.addEventListener("mouseover", () => {let randomColor = Math.floor(Math.random()*360);
+        gridCell.style.background = "hsl(randomColor, 100%, 50%)".replace(/randomColor/, randomColor);});
         gridRow.appendChild(gridCell);
     }
     gridRow.classList.add("grid-row");
